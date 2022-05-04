acba20fb643c3ed67fe949e967921ff69c0c526e
Daniel1987Daniel <83771369+Daniel1987Daniel@users.noreply.github.com>
2022-05-04 23:13:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var firstName = "Daniel";
+var lastName = "Sládeček";
+let sum  = "Ahojte, víta vás: " + firstName + " " + lastName;
+
+console.log(sum);
+console.log("Ahojte, víta vás: " + firstName + " " + lastName);
