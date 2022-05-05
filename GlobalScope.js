8a05304b149e7bc3f7c13b721303447c03d67464
Daniel1987Daniel <83771369+Daniel1987Daniel@users.noreply.github.com>
2022-05-05 22:08:48 +0200

    Update GlobalScope.js

--- GlobalScope.js
+++ GlobalScope.js
@@ -3,7 +3,7 @@ var galaxy = "The Milky Way";
 var stars = "North Star";
 
 function callMyNightSky() {
-  return 'Night Sky: ' + satellite + ', ' +      stars + ', and ' + galaxy;
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
 };
 
 console.log(callMyNightSky());
