73c95a2fafc88c3be5c98ccd6e6b5dbff21ef1e2
Daniel1987Daniel <83771369+Daniel1987Daniel@users.noreply.github.com>
2022-05-08 22:43:41 +0200

    Create Loops.js

---
+++ Loops.js
@@ -0,0 +1,8 @@
+const vacationSpots = ["Toronto", "Ottawa", "Usa"];
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+for(i = 0; i <= vacationSpots.length; i++) {
+  
+};
