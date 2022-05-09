d4bea3e61be9c58a80cc4c89b54ccab6ce398664
Daniel1987Daniel <83771369+Daniel1987Daniel@users.noreply.github.com>
2022-05-09 12:30:58 +0200

    Create LoopingThroughArrays.js

---
+++ LoopingThroughArrays.js
@@ -0,0 +1,7 @@
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+for (let i = 0; i < vacationSpots.length; i++){
+  console.log('I would love to visit ' +    vacationSpots[i]); // I would love to visit Bali
+                                                               // I would love to visit Paris
+                                                               // I would love to visit Tulum
+}
