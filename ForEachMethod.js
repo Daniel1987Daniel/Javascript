cffc9a47fd38df670db033d8017fe37f1806a87a
Daniel1987Daniel <83771369+Daniel1987Daniel@users.noreply.github.com>
2022-05-10 23:25:56 +0200

    Create ForEachMethod.js

---
+++ ForEachMethod.js
@@ -0,0 +1,9 @@
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(function(fruitsItem) {
+  console.log(`I want to eat a ` + fruitsItem);
+});     // I want to eat a mango
+        //  I want to eat a papaya
+       //  I want to eat a pineapple
+       //  I want to eat a apple
