0080214dd4c19246123e17d1ede35db25a43ab30
Daniel1987Daniel <83771369+Daniel1987Daniel@users.noreply.github.com>
2022-05-12 13:34:04 +0200

    Create FindIndexMethod.js

---
+++ FindIndexMethod.js
@@ -0,0 +1,5 @@
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+const foundAnimal = animals.findIndex(i => {
+  return i === "elephant";
+}); // index (i) 7
